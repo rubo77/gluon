fdb4b3876d7d2804aebb39b18e5e9c44b87d5ff0
rubo77 <github@r.z11.de>
2019-02-21 00:49:03 +0100

    extracted gluon-web-offline-ssid

--- package/gluon-web-offline-ssid/luasrc/lib/gluon/config-mode/model/admin/offline-ssid.lua
+++ package/gluon-web-offline-ssid/luasrc/lib/gluon/config-mode/model/admin/offline-ssid.lua
@@ -1,13 +1,13 @@
 local uci = require('simple-uci').cursor()
 local util = require 'gluon.util'
 
-local pkg_i18n = i18n 'gluon-offline-ssid'
+local pkg_i18n = i18n 'gluon-web-offline-ssid'
 
 local f = Form(pkg_i18n.translate('Offline-SSID'))
 
 local s = f:section(Section, nil, pkg_i18n.translate(
 	'Here you can enable to automatically change the SSID to the Offline-SSID '
-	.. 'when the node has no connection to the selected Gateway.'
+	.. 'when the node has no connection to the selected gateway.'
 ))
 
 local enabled = s:option(Flag, 'enabled', pkg_i18n.translate('Enabled'))
